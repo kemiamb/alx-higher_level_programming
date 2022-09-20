2c7d583d2de98a3da7a219ff95974ed49d45e554
Kemia Mbongo <kemiambongo@Kemias-MacBook-Pro.local>
2022-09-20 15:14:38 +0100

    last digit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord (str[i]) >= 97 and ord(str[i]) <= 122:
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             x = ord(str[i]) - 32
         else:
             x = ord(str[i])
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last = int(repr(number)[-1]) * -1
+    else
+        last = int(repr(number)[-1])
+    print(f"{last}", end='')
+    return last
