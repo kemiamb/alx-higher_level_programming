565526e0b0bca87e72f8329224cc4aab626b6d92
Kemia Mbongo <kemiambongo@Kemias-MacBook-Pro.local>
2022-09-20 15:03:15 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord (str[i]) >= 97 and ord(str[i]) <= 122:
+            x = ord(str[i]) - 32
+        else:
+            x = ord(str[i])
+        print("{:c}".format(x), end='')
+    print()
