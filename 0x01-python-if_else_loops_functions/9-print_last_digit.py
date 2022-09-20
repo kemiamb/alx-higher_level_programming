543ce16e80dd7a55563ea1ce7829ea05a7c8d766
Kemia Mbongo <kemiambongo@Kemias-MacBook-Pro.local>
2022-09-20 15:16:26 +0100

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         last = int(repr(number)[-1]) * -1
-    else
+    else:
         last = int(repr(number)[-1])
     print(f"{last}", end='')
     return last
