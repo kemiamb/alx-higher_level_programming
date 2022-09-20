2b933a7aa621267f6b88df71ae6b4e6c29d8356a
Kemia Mbongo <kemiambongo@Kemias-MacBook-Pro.local>
2022-09-20 13:15:06 +0100

    another alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(*["%c" % a for a in range(ord('a'),ord('z')+1) if a not in (ord('q'),ord('e'))],sep='',end='')
+for i in range(97, 123):
+    if i not in (113,101):
+        print("{:c}".format(i), end='')
