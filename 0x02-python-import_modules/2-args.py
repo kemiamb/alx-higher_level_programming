1e67a753a6c1e0cf3935eedede671a9681dfd909
Kemia Mbongo <kemiambongo@Kemias-MacBook-Pro.local>
2022-09-22 23:36:02 +0100

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv) - 1
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
 
-if count == 1:
-    print("{} argument:".format(count))
-elif count == 0:
-    print("{} arguments.".format(count))
-else:
-    print("{} arguments:".format(count))
+    if count == 1:
+        print("{} argument:".format(count))
+    elif count == 0:
+        print("{} arguments.".format(count))
+    else:
+        print("{} arguments:".format(count))
 
-if count >= 1:
-    count = 0
-    for arg in sys.argv:
-        if count != 0:
-            print("{}: {}".format(count, arg))
+    if count >= 1:
+        count = 0
+        for arg in sys.argv:
+            if count != 0:
+                print("{}: {}".format(count, arg))
         count += 1
