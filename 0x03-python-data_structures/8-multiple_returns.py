c65b6f19daad10ffc8fda541c345a911a75b942c
Kemia Mbongo <kemiambongo@Kemias-MacBook-Pro.local>
2022-09-27 13:12:02 +0100

    create tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if len(sentence) == 0:
         first = None
-        for i in range(len(sentence)):
-            i = 0
-            first = sentence[i]
+    for i in range(len(sentence)):
+        i = 0
+        first = sentence[i]
     return (len(sentence), first)
